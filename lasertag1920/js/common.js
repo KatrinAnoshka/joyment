684bf7e1ce96636d6d11d087d7ad8c33be8fbf74
KatrinAnoshka <nosha309@mail.ru>
2018-07-12 09:51:43 +0300

    fixes responsive div

---
+++ lasertag1920/js/common.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+/* Select */
+	$('.select').on('click',function(){
+		$(this).parent().find('.options').fadeIn('fast');
+	});
+	$('.options').on('mouseleave',function(){
+		$(this).fadeOut('fast');
+	});
+	$('.pseudo-select').on('mouseleave',function(){
+		$(this).find('.options').fadeOut('fast');
+	});
+	$('.options > div').on('click',function(){
+		//if ($(this).attr('value')) {
+			$(this).closest('.pseudo-select').find('.select').html($(this).text());
+			$(this).closest('.pseudo-select').find('input').attr('value', $(this).attr('value'));
+		//}
+		$.each($(this).parent().children('div.check'), function(){
+			$(this).removeClass('check');
+		});
+		$(this).addClass('check');
+		$(this).parent().fadeOut('fast');
+	});
+
+/* Slider-main */
+	
+	$('.carousel').carousel({
+      interval: 6000
+    })
+});
